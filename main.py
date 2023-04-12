60bce7890280afcfc4727d37accb51732c205a57
xXImrePVPXx <118882031+xXImrePVPXx@users.noreply.github.com>
2023-04-12 09:49:39 +0200

    vmi

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
-print('szia aron')
+import pygame#ezzel csinaljuk majd a jatekot
+pygame.init()
+bg = pygame.image.load("letöltés.jpg")
+bg = pygame.transform.scale(bg, (1024, 512))
+
+screen = pygame.display.set_mode([1024, 512])
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255,))
+    screen.blit(bg, (0, 0))
+
+    pygame.display.flip()
+
+pygame.quit()
