acfc3c5e6630d2a838cd2aee00a818a1642dacc9
bluenote45 <118980958+bluenote45@users.noreply.github.com>
2023-04-27 13:35:33 +0200

    changes

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import pygame #ezzel csinaljuk majd a jatekot
 import json
 from object import Kartya
 pygame.init()
-bg = pygame.image.load("letöltés.jpg")
+bg = pygame.image.load("kepek/letöltés.jpg")
 bg = pygame.transform.scale(bg, (1024, 512))
 pygame.display.set_caption('Zsírozás')
 with open("cards.json", "r", encoding="utf-8") as f:
@@ -19,18 +19,19 @@ p2points = []
 deck = [f]
 kard = Kartya(14, adat["Nev"], adat['szin'], adat['ertek'], adat['priority'], adat['kep'])
 print(kard)
-#running = True
-#while running:
-#
-#    for event in pygame.event.get():
-#        if event.type == pygame.QUIT:
-#            running = False
-#
-#    screen.fill((255, 255, 255,))
-#    screen.blit(bg, (0, 0))
-#    kard = Kartya(14, adat["Nev"], adat['szin'], adat['ertek'], adat['priority'], adat['kep'])
-#
-#
-#   pygame.display.flip()
-#
-#pygame.quit()
+
+running = True
+while running:
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    screen.fill((255, 255, 255,))
+    screen.blit(bg, (0, 0))
+    kard = Kartya(14, adat["Nev"], adat['szin'], adat['ertek'], adat['priority'], adat['kep'])
+
+
+    pygame.display.flip()
+
+pygame.quit()
