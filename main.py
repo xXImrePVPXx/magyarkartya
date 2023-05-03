e41ec4b4198416dd59bd441dd4c2bfea5642310b
xXImrePVPXx <118882031+xXImrePVPXx@users.noreply.github.com>
2023-05-03 07:44:46 +0200

    Szepseg, commentek + kis bugfix
    commenteket raktam bele hogy egyszerubb legyen elmagyarazni, ill. lehet mostmar csonkitani is(nincs a masiknak pontja), es vegul, hogyha 1 kartyad van nem azt irja ki hogy 4 kozul tudsz valasztani.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import random
 
-class Kartya:
+class Kartya:#Feldolgozza a fájlt
     def __init__(self, sor):
         id, nev, szin, ertek, priority = sor.split(';')
         self.id = int(id)
@@ -10,7 +10,7 @@ class Kartya:
         self.priority = int(priority)
 
     def __repr__(self):
-        return f'{self.nev}'
+        return f'|{self.nev}|'
 
 
 cards = []
@@ -23,7 +23,7 @@ def getcard(id):
     for i in cards:
         if i.id == id:
             return i
-    return 'card'+str(id)
+    return 'card'+str(id)#számot "id"-vé tud alakítani
 p1h = []
 aih = []
 p1t = []
@@ -33,9 +33,9 @@ pakli = []
 for i in range(10000):
    r=random.randint(1,32)
    if r not in pakli:
-      pakli.append(int(r))
+      pakli.append(int(r))#megkeveri a paklit
 
-pakli = list(map(getcard, pakli))
+pakli = list(map(getcard, pakli))#a paklinak a számait "id"-vé azaz kártyákká alakítjuk
 
 
 
@@ -43,10 +43,10 @@ aipont=0
 p1pont=0
 aiw = False
 
-print('Magyar Kártya:')
+print('Magyar Kártya:')#hoppa szabalyok
 print('ZSÍROZÁS')
 szab = input('Ismered a szabályokat?(Igen/Nem)')
-if szab == 'Nem':
+if szab == 'Nem' or szab == 'nem' or szab == 'n' or szab == 'N':
     print('Szabályok:')
     print('Minden játékos 4 lappal kezd')
     print('Az ászt és 10-est kell gyűjteni.')
@@ -55,7 +55,7 @@ if szab == 'Nem':
     print('Aki ütött, az rak legközelebb.')
     print('Aki hamarabb összegyűjti az 5 pontot, nyer.')
     print('----------------------')
-elif szab == 'Igen':
+elif szab == 'Igen' or szab == 'igen' or szab == 'i' or szab == 'I':
     print('Akkor kezdődjön a játék!')
 else:
     print('Ezt egy Nemnek veszem.')
@@ -69,91 +69,98 @@ else:
     print('----------------------')
 
 
-p1h = pakli[:4]
+p1h = pakli[:4]#Kiosztja a kártyát
 print('Kártyáid:')
 print(*p1h)
 del pakli[:4]
-aih = pakli[:4]
+aih = pakli[:4]#A robotnak is kiosztja
 del pakli[:4]
-while aipont < 5 and p1pont < 5:
-    if aiw:
-        aicard = aih[0]
-        del aih[0]
-
-        print('Robot által lerakott kártya', end=':')
-        print(aicard.nev)
-    a = int(input('Válassz egy kártyát(1,2,3,4):'))
-    selectedcard = p1h[a-1]
-    del p1h[a-1]
-    if not aiw:
-        aicard = aih[0]
-        del aih[0]
-
-    if selectedcard.szin == aicard.szin or selectedcard.priority == 1 or aicard.priority == 1:
-        if selectedcard.priority < aicard.priority or selectedcard.priority == 1:
-            p1t.append(selectedcard)
-            p1t.append(aicard)
-            print('Játékos vitte.')
-            print('---------------------')
-            print(f'Pontjaid:{p1pont}')
-            aiw = False
-            print('Pakli:')
-            print(len(pakli))
-        elif selectedcard.priority > aicard.priority or aicard.priority == 1:
-            ait.append(selectedcard)
-            ait.append(aicard)
-            print('Robot vitte.')
-            print('---------------------')
-            print(f'Pontjaid:{p1pont}')
-            aiw = True
-            print('Pakli:')
-            print(len(pakli))
-    else:
-        if aiw:
-            ait.append(selectedcard)
-            ait.append(aicard)
-            print('Robot vitte.')
-            print('---------------------')
-            print(f'Ponjaid:{p1pont}')
-            aiw = True
-            print('Pakli:')
-            print(len(pakli))
-        if not aiw:
-            p1t.append(selectedcard)
-            p1t.append(aicard)
-            print('Játékos vitte.')
-            print('---------------------')
-            print(f'Ponjaid:{p1pont}')
-            aiw = False
-            print('Pakli:')
-            print(len(pakli))
-
-    if len(p1h) < 4:
+while True:
+    if len(p1h) < 4:#Húzz kártyát
         p1h = p1h[:3] + pakli[:1]
         del pakli[:1]
         print('Kártyáid:')
         print(*p1h)
 
-    if len(aih) < 4:
+    if len(aih) < 4:#Húzz kártyát
         aih = aih[:3] + pakli[:1]
         del pakli[:1]
+    if aiw:
+        aicard = aih[0]
+        del aih[0]
+        print('Robot által lerakott kártya', end=':')
+        print(aicard.nev)
+    if len(p1h)==4:
+        a = int(input('Válassz egy kártyát(1,2,3,4):'))
+        selectedcard = p1h[a-1]#Kártyát lerakja a játékos(4)
+    if len(p1h)==3:
+        a = int(input('Válassz egy kártyát(1,2,3):'))
+        selectedcard = p1h[a-1]#Kártyát lerakja a játékos(3)
+    if len(p1h)==2:
+        a = int(input('Válassz egy kártyát(1,2):'))
+        selectedcard = p1h[a-1]#Kártyát lerakja a játékos(2)
+    if len(p1h)==1:
+        a = int(input('Válassz egy kártyát(1):'))
+        selectedcard = p1h[a-1]#Kártyát lerakja a játékos(1)
+    del p1h[a-1]
+    if not aiw:
+        aicard = aih[0]
+        del aih[0]
+        print('Robot által lerakott kártya', end=':')
+        print(aicard.nev)
 
-
-    p1pont = 0
+    if selectedcard.szin == aicard.szin or selectedcard.priority == 1 or aicard.priority == 1:#Megnézi hogy a szín ugyan az-e vagy az egyik hetes e
+        if selectedcard.priority < aicard.priority or selectedcard.priority == 1:#Hogyha a játékos nyert
+            p1t.append(selectedcard)
+            p1t.append(aicard)
+            print('Játékos vitte.')
+            print('---------------------')
+            print(f'Pontjaid:{p1pont}')
+            aiw = False
+            print('Pakli:')
+            print(len(pakli))
+        elif selectedcard.priority > aicard.priority or aicard.priority == 1:#Hogyha a bot nyert
+            ait.append(selectedcard)
+            ait.append(aicard)
+            print('Robot vitte.')
+            print('---------------------')
+            print(f'Pontjaid:{p1pont}')
+            aiw = True
+            print('Pakli:')
+            print(len(pakli))
+    else:#Ha nem ugyan az a szín
+        if aiw:#Ha nyert a bot
+            ait.append(selectedcard)
+            ait.append(aicard)
+            print('Robot vitte.')
+            print('---------------------')
+            print(f'Ponjaid:{p1pont}')
+            aiw = True
+            print('Pakli:')
+            print(len(pakli))
+        if not aiw:#Ha nem nyert
+            p1t.append(selectedcard)
+            p1t.append(aicard)
+            print('Játékos vitte.')
+            print('---------------------')
+            print(f'Ponjaid:{p1pont}')
+            aiw = False
+            print('Pakli:')
+            print(len(pakli))
+    if pakli == [] and p1h ==[] and aih ==[]: #Ha elfogytak a kártyák
+        print('Elfogytak a kártyák.')
+        break
+    p1pont = 0#Pontot számol
     for i in p1t:
         p1pont += i.ertek
 
-    aipont = 0
+    aipont = 0#Pontot számol
     for i in ait:
         aipont += i.ertek
 
-    if pakli == [] and p1h ==[] and aih ==[]:
-        print('Elfogytak a kártyák.')
-        break
-
-if p1pont == 5:
+if p1pont>aipont:#Kinyert?
     print('Nyertél')
-elif aipont == 5:
+elif aipont>p1pont:
     print('Vesztetél.')
 else:
     print('Döntetlen.')
